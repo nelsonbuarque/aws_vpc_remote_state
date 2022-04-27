5f2a4b9ffa8603c896bd792214dbc4de5893d400
Nelson <nelson_buarque@hotmail.com>
2022-04-27 09:42:21 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+    backend "s3" {
+    bucket = "test-terraform-nelson"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+    default_tags {
+      tags = {
+          owner = "Nelson"
+          terraform = "true"
+      }
+    }
+
+}
