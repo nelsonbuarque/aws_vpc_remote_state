d667a4ea9aadcabf2630df1864ae8d4fb23b9392
Nelson <nelson_buarque@hotmail.com>
2022-04-27 09:42:49 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.11.0"
     }
   }
-    backend "s3" {
+  backend "s3" {
     bucket = "test-terraform-nelson"
     key    = "aws-vpc/terraform.tfstate"
     region = "eu-central-1"
@@ -15,11 +15,11 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 
-    default_tags {
-      tags = {
-          owner = "Nelson"
-          terraform = "true"
-      }
+  default_tags {
+    tags = {
+      owner     = "Nelson"
+      terraform = "true"
     }
+  }
 
 }
